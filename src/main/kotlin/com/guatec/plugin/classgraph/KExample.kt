8ffa104174741d30b3d4883af3c0f6e40b2c9a84
Juan Liska <jpliska@gmail.com>
2016-01-11 00:58:19 -0600

    add KExample and KExample tests

--- src/main/kotlin/com/guatec/plugin/classgraph/KExample.kt
+++ src/main/kotlin/com/guatec/plugin/classgraph/KExample.kt
@@ -3,3 +3,7 @@ package com.guatec.plugin.classgraph
 /**
  * Created by juanliska on 1/11/16.
  */
+
+class KExample{
+    val ok = true
+}
--- src/test/kotlin/com/guatec/plugin/classgraph/KExampleTest.kt
+++ src/test/kotlin/com/guatec/plugin/classgraph/KExampleTest.kt
@@ -1,8 +1,15 @@
 package com.guatec.plugin.classgraph
 
+import org.testng.annotations.*
 import org.testng.Assert.*
 
 /**
  * Created by juanliska on 1/11/16.
  */
-class KExampleTest
+class KExampleTest {
+    @Test
+    fun shouldBeOk(){
+        val ke = KExample()
+        assert(ke.ok)
+    }
+}
